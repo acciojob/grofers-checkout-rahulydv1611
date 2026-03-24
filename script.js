df3ff6085d02c04fdf259be76732bf1b66122f9a
Rahul Yadav <130846443+rahulydv1611@users.noreply.github.com>
2026-03-24 23:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
   
-};
+    const priceElements = document.querySelectorAll('.price');
+    let total = 0;
 
+    
+    priceElements.forEach(element => {
+       
+        total += parseFloat(element.textContent);
+    });
+
+
+    const table = document.querySelector('table');
+    const newRow = document.createElement('tr');
+    const newCell = document.createElement('td');
+
+    newCell.textContent = total;
+    newCell.setAttribute('id', 'ans');
+    
+    newRow.appendChild(newCell);
+    table.appendChild(newRow);
+};
 getSumBtn.addEventListener("click", getSum);
 
