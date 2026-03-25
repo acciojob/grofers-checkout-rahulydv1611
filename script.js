b7d32fec5569f40e2641a1adddbec4822fd7a6c2
Rahul Yadav <130846443+rahulydv1611@users.noreply.github.com>
2026-03-25 16:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function calculateTotal() {
+let button = document.querySelector("button");
+
+button.addEventListener("click", () => {
     let prices = document.querySelectorAll(".prices");
     let total = 0;
 
     prices.forEach((price) => {
         let value = price.textContent.trim();
 
-        // Extract only last number (handles Cypress typing)
-        let num = value.match(/\d+$/); 
+        // Take only last number (handles Cypress typing issue)
+        let num = value.match(/\d+$/);
+
         total += Number(num);
     });
 
     document.getElementById("ans").textContent = total;
-}
+});
